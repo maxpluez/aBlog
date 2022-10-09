92413e536521cc017b6578bebe701f2492c34ec9
Ubuntu <ubuntu@ip-172-31-83-162.ec2.internal>
2022-10-09 03:30:07 +0000

    Update some info

--- routes/insurmountable.js
+++ routes/insurmountable.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-  res.redirect('http://54.174.197.121:8000');
+  res.redirect('http://zhangmax.com:8000');
 });
 
 module.exports = router;
